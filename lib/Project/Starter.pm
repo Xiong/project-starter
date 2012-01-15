a3c94969719085c71e82beeec85e656fc483f728
Xiong Changnian <xiong@cpan.org>
2012-01-15 01:04:02 -0800

    New.
    * Created some files from module-starter.
    * Copied some from error-base.
    * If this seems sloppy... this is why project-starter.

---
+++ lib/Project/Starter.pm
@@ -0,0 +1,127 @@
+package Project::Starter;
+#=========# MODULE USAGE
+use Project::Starter;       # !------44 chars max tagline in PAUSE upload!
+                            # Start stub dirs, templates, flexible config
+
+#~ use 5.008008;       # 5.8.8
+#~ use 5.010001;       # 5.10.1
+#~ use 5.012003;       # 5.12.3
+#~ use 5.014002;       # 5.14.2
+use strict;
+use warnings;
+use version; our $VERSION = qv('0.0.0');
+
+# Core modules
+
+# CPAN modules
+
+# Alternate uses
+#~ use Devel::Comments '#####', ({ -file => 'log/debug.log' });             #~
+
+## use
+#============================================================================#
+
+
+
+
+
+
+
+#============================================================================#
+######## END MODULE #########
+1;
+__END__
+
+=head1 NAME
+
+Project::Starter - xxxTAGLINExxx
+
+=head1 VERSION
+
+This document describes Project::Starter version 0.0.0
+
+=head1 SYNOPSIS
+
+    use Project::Starter;
+
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head1 DIAGNOSTICS
+
+=over
+
+=item C<< Error message here, perhaps with %s placeholders >>
+
+[Description of error here]
+
+=item C<< Another error message here >>
+
+[Description of error here]
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=for author to fill in:
+    A full explanation of any configuration system(s) used by the
+    module, including the names and locations of any configuration
+    files, and the meaning of any environment variables or properties
+    that can be set. These descriptions must also include details of any
+    configuration language used.
+  
+Project::Starter requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+=for author to fill in:
+    A list of all the other modules that this module relies upon,
+    including any restrictions on versions, and an indication whether
+    the module is part of the standard Perl distribution, part of the
+    module's distribution, or must be installed separately. ]
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+=for author to fill in:
+    A list of any modules that this module cannot be used in conjunction
+    with. This may be due to name conflicts in the interface, or
+    competition for system or program resources, or due to internal
+    limitations of Perl (for example, many modules that use source code
+    filters are mutually incompatible).
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+=for author to fill in:
+    A list of known problems with the module, together with some
+    indication Whether they are likely to be fixed in an upcoming
+    release. Also a list of restrictions on the features the module
+    does provide: data types that cannot be handled, performance issues
+    and the circumstances in which they may arise, practical
+    limitations on the size of data sets, special cases that are not
+    (yet) handled, etc.
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-project-starter@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Xiong Changnian  C<< <xiong@cpan.org> >>
+
+=head1 LICENSE
+
+Copyright (c) 2012, Xiong Changnian C<< <xiong@cpan.org> >>. 
+All rights reserved. No warranty.
+
+This library and its contents are released under Artistic License 2.0:
+
+L<http://www.opensource.org/licenses/artistic-license-2.0.php>
+
+=cut
